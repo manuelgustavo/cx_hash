7045ac1fdd0267ac912c753d369470df8251419a
manuelgustavo <manuel.gustavo@gmail.com>
2017-04-06 12:06:01 +0100

    better architecture handling.

--- cx_hash/cx_hash.cpp
+++ cx_hash/cx_hash.cpp
@@ -2,22 +2,17 @@
 #include <cassert>
 using namespace std;
 
+
 constexpr size_t cx_hash(const char* input) {
-#if defined(_WIN64)
-	static_assert(sizeof(size_t) == 8, "size_t is not 64 bits long!");
-	size_t hash = 0xcbf29ce484222325;
-	const size_t prime = 0x00000100000001b3;
-#else 
-	static_assert(sizeof(size_t) == 4, "size_t is not 32 bits long!");
-	size_t hash = 0x811c9dc5;
-	const size_t prime = 0x01000193;
-#endif
+	size_t hash = sizeof(size_t) == 8 ? 0xcbf29ce484222325 : 0x811c9dc5;
+	const size_t prime = sizeof(size_t) == 8 ? 0x00000100000001b3 : 0x01000193;
 
 	while (*input) {
 		hash ^= static_cast<size_t>(*input);
 		hash *= prime;
 		++input;
 	}
+
 	return hash;
 }
 
